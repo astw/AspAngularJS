315ef09a9666224c232443cd9f68766798889a1b
Shuhao <wshuhao@gmail.com>
2014-10-16 09:22:20 -0700

    add jquery and animated integration

---
+++ AspAngularJS/AspAngularJS/js/angular/directives/datePicker.js
@@ -0,0 +1,20 @@
+﻿'use restrict',
+
+(function () {
+
+    var app = angular.module("PhotoApp");
+    var datepicker = function () {
+        return {
+            restroct: 'A',
+            link: function (scope, element) {
+                element.datepicker();
+            }
+
+            
+        };
+    };
+
+    app.directive("datePicker", datepicker);
+
+
+}())
